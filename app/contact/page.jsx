f22318484227bd6c0b38e86dcaa4487c8ee4a74a
Youssef Jnah <youssefj86@gmail.com>
2024-12-10 18:18:30 +0100

    lier le formulaire avec Formspark

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <form action="https://submit-form.com/Liltai6Jd" className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-3xl text-accent">Travaillons ensemble</h3>
               <p className="text-white/60">N&apos;hésitez pas à me contacter, je suis ouvert à toute opportunité de travail qui correspond à mes compétences et à mes intérêts.</p>
               {/* input */}
